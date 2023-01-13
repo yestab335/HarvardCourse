ddd790e5d32a876c546355bcd9a25668f4dc8aa3
Yasser Tabora <superguy723@Yassers-MacBook-Pro.local>
2023-01-13 13:58:46 -0500

    Refactored Code Tab Spacing And Added New Routes For Auction

--- project-2/auctions/urls.py
+++ project-2/auctions/urls.py
@@ -2,17 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
-    path("auction/new", views.newListing, name="newListing"),    
-    path("auction/active", views.activeListings, name="activeListings"),
-    path("auction/active/<int:category_id>", views.activeListings, name="activeListings"),
-    path("auction/watchlist", views.watchlist, name="watchlist"),
-    path("auction/watchlist/<int:listing_id>/change/<str:reverse_method>", views.change_watchlist, name="change_watchlist"),
-    path("auction/listing/<int:listing_id>", views.listing, name="listing"),
-    path("auction/listing/<int:listing_id>/bid", views.take_bid, name="take_bid"),    
-    path("auction/listing/<int:listing_id>/close", views.close_listing, name="close_listing"),
-    path("auction/listing/<int:listing_id>/comment", views.comment, name="comment"),
+  path("", views.index, name="index"),
+  path("auction/<str:id>", views.auction, name="auction"),
+  path("auction/<str:id>/bid", views.auction_bid, name="auction_bid"),
+  path("auction/<str:id>/close", views.auction_close, name="close_auction"),
+  path("auction/<str:id>/comment", views.auction_comment, name="post_comment"),
+  path("auction/<str:id>/watch", views.watch_auction, name="watch_auction"),
+  path("categories", views.categories_list, name="categories"),
+  path("categories/<str:name>", views.category, name="category"),
+  path("create_listing", views.create_listing, name="create_listing"),
+  path("login", views.login_view, name="login"),
+  path("logout", views.logout_view, name="logout"),
+  path("register", views.register, name="register"),
+  path("watchlist", views.watchlist, name="watchlist")
 ]
