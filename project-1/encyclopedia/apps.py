1fea9ba340eed13d00a6e52ec313751a81ccfc36
Yasser Tabora <superguy723@Yassers-MacBook-Pro.local>
2023-03-01 18:54:28 -0500

    Refactored Code Spacing From 4 To 2

--- project-1/encyclopedia/apps.py
+++ project-1/encyclopedia/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class EncyclopediaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'encyclopedia'
+  default_auto_field = 'django.db.models.BigAutoField'
+  name = 'encyclopedia'
